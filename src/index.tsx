dcffbb2fec359d003fdef3c4020dca7ed9a87192
Jainil Prajapati <github-jainil@1209.in>
2026-08-12 16:56:56 +0530

    chore: enhance performance by adding DNS prefetch and preconnect links

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@fontsource-variable/manrope";
-import "@fontsource-variable/outfit";
-import "@fontsource-variable/playfair-display";
+import "@fontsource-variable/manrope/wght.css";
+import "@fontsource-variable/outfit/wght.css";
+import "@fontsource-variable/playfair-display/wght.css";
 import "@/index.css";
 import App from "@/App";
 
